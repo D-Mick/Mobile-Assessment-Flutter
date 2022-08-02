c0b455b58e0b4275e89d2db60da1dcb22e5d3ee1
Durotimi <timi@weprototype.co>
2022-08-02 16:37:12 +0100

    Added images to assest folder and added new dependencies to pubspec

---
+++ lib/ui/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Shipify extends StatelessWidget {
+  const Shipify({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
